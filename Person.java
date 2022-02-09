905ca6aec2a4c8f3255301d6fb59d2e6b738613b
sunchi <rachanlamoolphal@gmail.com>
2022-02-09 22:52:28 +0700

    Person update

--- Person.java
+++ Person.java
@@ -2,11 +2,6 @@ public class Person{
     protected String name;
     protected int age;
 
-    public Person(String _name, int _age){
-        this.name = _name;
-        this.age = _age;
-    }
-
     private int getAge(){
         return this.age;
     }
